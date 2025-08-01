13a5c579eb51c829f39a38f1b47eae7219513938
Arctic Fox <tyskanhik@mail.ru>
2025-08-01 20:01:39 +0300

    create repository

---
+++ server/src/main/java/com/example/server/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.example.server.repository;
+
+import com.example.server.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findAll();
+}
