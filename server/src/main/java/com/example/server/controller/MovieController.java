7f1f62ff8e832d916908fac722f6052a0dae038e
Arctic Fox <tyskanhik@mail.ru>
2025-08-07 15:23:59 +0300

    edit controller add post method

--- server/src/main/java/com/example/server/controller/MovieController.java
+++ server/src/main/java/com/example/server/controller/MovieController.java
@@ -3,9 +3,19 @@ package com.example.server.controller;
 import com.example.server.dto.MovieDto;
 import com.example.server.service.MovieService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,5 +27,18 @@ public class MovieController {
     @GetMapping
     public List<MovieDto> getAllMovies() {
         return movieService.getAllMovies();
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<MovieDto> createMovie(
+            @RequestPart("movie") @Validated MovieDto movieDto,
+            @RequestPart("poster") MultipartFile posterFile) {
+        
+        try {
+            MovieDto createdMovie = movieService.createMovie(movieDto, posterFile);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdMovie);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
